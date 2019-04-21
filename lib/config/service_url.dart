62bb57bdf8bf51412607b42cd4bea3b43d0af46c
zoujing <zoujing_jean@163.com>
2019-04-22 00:11:43 +0800

    first commit

---
+++ lib/config/service_url.dart
@@ -0,0 +1,6 @@
+const serviceUrl = 'https://wxmini.baixingliangfan.cn/baixing/';
+const servicePath = {
+  'homePageContext': serviceUrl + 'wxmini/homePageContent', // 商家首页信息
+  'homePageBelowConten': serviceUrl+'wxmini/homePageBelowConten', // 商城首页热卖商品拉取
+  'getCategory': serviceUrl+'wxmini/getCategory', //商品类别信息
+};
---
+++ lib/model/category.dart
@@ -0,0 +1,58 @@
+class CategoryModel {
+  List data;
+  String code;
+  String message;
+
+  CategoryModel({
+    this.data,
+    this.code,
+    this.message
+  });
+
+  factory CategoryModel.fromJson(dynamic json) {
+    return CategoryModel(
+      data: json['data'],
+      code: json['code'],
+      message: json['message']
+    );
+  }
+}
+
+class CategoryBigModel {
+  String mallCategoryId; // 类别编号
+  String mallCategoryName; // 类别名称
+  List<dynamic> bxMallSubDto; // 小类列表
+  String image; // 类别图片
+  Null comments; // 列表描述
+
+  CategoryBigModel({
+    this.mallCategoryId,
+    this.mallCategoryName,
+    this.bxMallSubDto,
+    this.image,
+    this.comments
+  });
+
+  factory CategoryBigModel.fromJson(dynamic json){
+    return CategoryBigModel(
+      mallCategoryId: json['mallCategoryId'],
+      mallCategoryName: json['mallCategoryName'],
+      bxMallSubDto: json['bxMallSubDto'],
+      image: json['image'],
+      comments: json['comments'],
+    );
+  }
+}
+
+class CategoryBigListModel{
+  List<CategoryBigModel> data;
+
+  CategoryBigListModel(this.data);
+
+  factory CategoryBigListModel.fromJson(List json){
+    return CategoryBigListModel(
+      json.map((item){return CategoryBigModel.fromJson(item);}).toList()
+    );
+  }
+}
+
